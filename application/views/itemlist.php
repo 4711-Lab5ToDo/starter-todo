de105e66a31594d524edfaa5b976af4c25d30aa7
Nick Nelson <nick34nelson@hotmail.com>
2017-02-28 16:19:34 -0800

    Task 6 completed.

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -4,11 +4,12 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+{pagination}
 <table class="table">
         <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Status</th>
+            <th>Id</th>
+            <th>Task</th>
+            <th>Status</th>
         </tr>
-        {display_tasks} 
+        {display_tasks}
 </table>
