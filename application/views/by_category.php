38b6fe66b87c5905d68fb65bd8747f9da589e5ac
Jimmy Wang <jimmywang2142@gmail.com>
2017-03-07 16:40:27 -0800

    added checkbox control to homepage, job 10

--- application/views/by_category.php
+++ application/views/by_category.php
@@ -11,19 +11,23 @@ and open the template in the editor.
     </head>
     <body>
         <h3>Tasks by Category</h3>
-        <table class="table">
-            <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Category</th>
-            </tr>
-            {display_tasks}
-            <tr>
-                <td>{id}</td>
-                <td>{task}</td>
-                <td>{category}</td>
-            </tr>
-            {/display_tasks}    
-        </table>
+        <form method='POST' action='{completer}'>
+            <table class="table">
+                <tr>
+                    <th>Id</th>
+                    <th></th>
+                    <th>Task</th>
+                    <th>Category</th>
+                </tr>
+                {display_tasks}
+                <tr>
+                    <td>{id}</td>
+                    <td><input type='checkbox' name='task{id}' /></td>
+                    <td>{task}</td>
+                    <td>{group}</td>
+                </tr>
+                {/display_tasks}    
+            </table>
+        <input type='submit' value='Complete check tasks' />
     </body>
 </html>
